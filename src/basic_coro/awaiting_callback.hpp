0866b41fb3e28423709bfa649ba8e43748f74634
Ondřej Novák <ondra-novak@email.cz>
2025-03-25 17:54:12 +0100

    awaiting callback is copyable but has no effect

--- src/basic_coro/awaiting_callback.hpp
+++ src/basic_coro/awaiting_callback.hpp
@@ -8,11 +8,11 @@ namespace coro {
 
 /// Constructs a class which can be used to store a callback function while awaiting on an awaitable
 /** In contrast to standard coroutine, this class has known size during compile time and its instance
- * can be created as member variable of another class. 
- * 
- * @param Awaiter type of standard C++ awaiter. It must be direct awaiter, you need to call 
+ * can be created as member variable of another class.
+ *
+ * @param Awaiter type of standard C++ awaiter. It must be direct awaiter, you need to call
  *      operator co_await() manually if necessery. Note that Awaiter must be move constructible
- *      because it is moved into the instance. 
+ *      because it is moved into the instance.
  * @param ClosureArgsExample an example list of closure arguments for the callback. This helps
  *      to calculate required space. The actual closure can be differnt as long as its
  *      size is less or equal to calculated space
@@ -44,7 +44,7 @@ class awaiting_callback {
 
     ///contains example callback function with specified closure
     struct ExampleCB {
-        std::tuple<ClosureArgsExample...> _closure;        
+        std::tuple<ClosureArgsExample...> _closure;
         void operator()(Awaiter &) {
             throw invalid_state();
         }
@@ -59,31 +59,34 @@ public:
 
     ///construct the callback space
     awaiting_callback() {};
-    ///cannot be copied
-    awaiting_callback(const awaiting_callback &) = delete;
-    ///cannote be copied
-    awaiting_callback &operator=(const awaiting_callback &) = delete;
+    ///object can be copied, but because it is frame, no content is copied
+    awaiting_callback(const awaiting_callback &)
+        :_frame_charged(false),_awaiter_charged(false) {}
+    ///assigment has no effect, cannot copy internal state
+    awaiting_callback &operator=(const awaiting_callback &) {
+        return *this;
+    }
     ///dtor
     ~awaiting_callback() {
         clear_frame();
         clear_awaiter();
     }
-    
+
     ///continue in await operation
     /**
-     * Starts await operation which is contination of previous await operation. 
+     * Starts await operation which is contination of previous await operation.
      * This function expects that callback has been set by function await(). This
      * function can be used inside of the callback to repeat await operation (the
      * callback is called again when await operation is complete)
-     * 
+     *
      * @param awt awaiter to await on
-     * @return prepared coro holding result of await_suspend operation 
+     * @return prepared coro holding result of await_suspend operation
      *  if a handle is returned. It can return handle to callback if
-     *  the operation is already complete. You should resume the 
+     *  the operation is already complete. You should resume the
      *  prepared_coro in order to finish asynchronous operation
-     * 
+     *
      * @exception invalid_state await_cont() called without initial await()
-     * 
+     *
      */
     prepared_coro await_cont(Awaiter &awt) {
         clear_awaiter();
@@ -99,18 +102,18 @@ public:
 
     ///continue in await operation
     /**
-     * Starts await operation which is contination of previous await operation. 
+     * Starts await operation which is contination of previous await operation.
      * This function expects that callback has been set by function await(). This
      * function can be used inside of the callback to repeat await operation (the
      * callback is called again when await operation is complete)
-     * 
+     *
      * @param awt awaiter to await on
-     * @return prepared coro holding result of await_suspend operation 
+     * @return prepared coro holding result of await_suspend operation
      *  if a handle is returned. It can return handle to callback if
-     *  the operation is already complete. You should resume the 
+     *  the operation is already complete. You should resume the
      *  prepared_coro in order to finish asynchronous operation
      * @exception invalid_state await_cont() called without initial await()
-     * 
+     *
      */
     prepared_coro await_cont(Awaiter &&awt) {
         return await_cont(awt);
@@ -119,12 +122,12 @@ public:
     ///initiate await operation
     /**
      * @param awt awaiter to await on
-     * @param cb callback with a closure. The closure must occupy less or equal calculated space 
-     * @return prepared coro holding result of await_suspend operation 
+     * @param cb callback with a closure. The closure must occupy less or equal calculated space
+     * @return prepared coro holding result of await_suspend operation
      *  if a handle is returned. It can return handle to callback if
-     *  the operation is already complete. You should resume the 
+     *  the operation is already complete. You should resume the
      *  prepared_coro in order to finish asynchronous operation
-     * 
+     *
      * @note it is allowed to reuse this object to another await operation. You only
      * need to prevent to initiate await operation while other is still pending
      */
@@ -140,15 +143,15 @@ public:
     ///initiate await operation
     /**
      * @param awt awaiter to await on
-     * @param cb callback with a closure. The closure must occupy less or equal calculated space 
-     * @return prepared coro holding result of await_suspend operation 
+     * @param cb callback with a closure. The closure must occupy less or equal calculated space
+     * @return prepared coro holding result of await_suspend operation
      *  if a handle is returned. It can return handle to callback if
-     *  the operation is already complete. You should resume the 
+     *  the operation is already complete. You should resume the
      *  prepared_coro in order to finish asynchronous operation
-     * 
+     *
      * @note it is allowed to reuse this object to another await operation. You only
      * need to prevent to initiate await operation while other is still pending
-     
+
      */
     template<std::invocable<Awaiter &> CB>
     requires (sizeof(coro_frame_cb<CallCB<CB> >) <= required_space)
@@ -158,9 +161,9 @@ public:
 
     ///clear a coroutine frame, call destructor on the callback
     /**
-     * If the instance was previously used for awaiting, 
+     * If the instance was previously used for awaiting,
      * this function destroys stored callback function (calls its destructor)
-     * You can call this function only if operation is already complete 
+     * You can call this function only if operation is already complete
      */
     void clear_frame() {
         if (_frame_charged) {
@@ -172,7 +175,7 @@ public:
     /**
      * Stored awaiter is clear, its destructor is called. This can be important
      * for some types of awaiters
-     * You can call this function only if operation is already complete 
+     * You can call this function only if operation is already complete
      */
     void clear_awaiter() {
         if (_awaiter_charged) {
@@ -183,9 +186,9 @@ public:
 
     ///clear object state
     /**
-     * Clear both awaiter and frame, call their destructors. 
-     * 
-     * In normal usage, you don't need to clear state explicitly. 
+     * Clear both awaiter and frame, call their destructors.
+     *
+     * In normal usage, you don't need to clear state explicitly.
      * Functions await and await_cont handles clear automatically
      */
     void clear() {
@@ -198,7 +201,7 @@ protected:
         Awaiter _awt;
     };
     union {
-        ExampleFrame _frame;        
+        ExampleFrame _frame;
         char _space[required_space];
     };
     bool _frame_charged = false;
